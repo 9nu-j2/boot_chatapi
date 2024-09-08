b96471023637f441b086daf98649299db071f97d
JUN9 <jungu269@gmail.com>
2024-09-08 17:24:36 +0900

    setting: 초기 설정 반영

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -1,30 +1,22 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.SalesGroupBy;
-import com.example.demo.service.SalesService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RequestMapping("/api")
+@RequestMapping("/chat")
 @Slf4j
 @RestController
-public class ApiController {
-    @Autowired
-    private SalesService salesService;
+public class ChatController {
 
     //GET
-    @GetMapping("/sales")
+    @GetMapping("/enter")
     @Tag(name="세일즈 데이터 집계", description = "세일즈 데이터 집계 조회")
     @Operation(summary="카테고리 개수, 평균 가격/개수 조회", description = "카테고리 개수, 평균 가격/개수 조회")
-    public List<SalesGroupBy> Sales() {
-        //조회해서 뿌리기
-        return salesService.getSalesGroupByDate();
+    public void Sales() {
+
     }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import com.example.demo.dto.SalesGroupByInter;
-import com.example.demo.repository.SalesRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,13 +10,6 @@ import java.util.List;
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Autowired
-    private SalesRepository salesRepository;
 
-	@Test
-	void contextLoads() {
-		List<SalesGroupByInter> res = salesRepository.findGroupbySalesData();
-
-	}
 
 }

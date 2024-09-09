c0ca9112688a52c6d7ac11e0118fe7a185a41a13
2-jun9 <jungu269@gmail.com>
2024-09-09 10:39:20 +0900

    add: 웹소켓 관련 기능 추가

---
+++ src/main/java/com/example/demo/entity/ChatRoom.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+public class ChatRoom {
+    private String roomId;
+    private String name;
+    private Set<WebSocketSession> sessions = new HashSet<>();
+    @Builder
+    public ChatRoom(String roomId, String name) {
+        this.roomId = roomId;
+        this.name = name;
+    }
+}

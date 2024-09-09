e1c045e36f1b9d2e83d1bf27a56d68138e2f392e
2-jun9 <jungu269@gmail.com>
2024-09-09 17:42:29 +0900

    add: chat 기능 수정

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.ChatRoom;
+import com.example.demo.dto.ChatRoom;
 import com.example.demo.service.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin()
 @RestController
 @RequestMapping("/chat")
 @RequiredArgsConstructor
@@ -22,19 +30,26 @@ public class ChatController {
         return "chat/chatList";
     }
 
-
-    @PostMapping("/createRoom")  //방을 만들었으면 해당 방으로 가야지.
-    public String createRoom(Model model, @RequestParam String name, String username) {
-        ChatRoom room = chatService.createRoom(name);
-        model.addAttribute("room",room);
-        model.addAttribute("username",username);
-        return "chat/chatRoom";  //만든사람이 채팅방 1빠로 들어가게 됩니다
+    @Operation(summary = "Create a new chat", description = "create new chatroom")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "chat created successfully",
+                    content = @Content(schema = @Schema(implementation = ChatRoom.class)),
+                    headers = @Header(name = "Location", description = "The URI of the newly created member", schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @PostMapping
+    public ResponseEntity<ChatRoom> createRoom(@RequestParam String location) {
+        ChatRoom room = chatService.createRoomIfNotExist(location);
+        return ResponseEntity.status(201).header("Location", "/chat/" ).body(room);  //만든사람이 채팅방에 먼저 들어감
     }
 
-    @GetMapping("/chatRoom")
-    public String chatRoom(Model model, @RequestParam String roomId){
-        ChatRoom room = chatService.findRoomById(roomId);
-        model.addAttribute("room",room);   //현재 방에 들어오기위해서 필요한데...... 접속자 수 등등은 실시간으로 보여줘야 돼서 여기서는 못함
-        return "chat/chatRoom";
-    }
+//    @GetMapping("/{location}")
+//    public ResponseEntity<ChatRoom> chatRoom(Model model, @RequestParam String location){
+//        ChatRoom room = chatService.findOrCreateChatRoom(location);
+//
+//        return ResponseEntity.ok();
+//    }
 }
--- src/main/java/com/example/demo/service/ChatService.java
+++ src/main/java/com/example/demo/service/ChatService.java
@@ -1,14 +1,19 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.ChatRoom;
+import com.example.demo.dto.ChatRoom;
+import com.example.demo.entity.ChatRoomEntity;
+import com.example.demo.repository.ChatRoomRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import groovy.util.logging.Slf4j;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.*;
-
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -16,6 +21,8 @@ public class ChatService {
 
     private final ObjectMapper objectMapper;
     private Map<String, ChatRoom> chatRooms;
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
 
     @PostConstruct
     private void init() {
@@ -30,13 +37,31 @@ public class ChatService {
         return chatRooms.get(roomId);
     }
 
-    public ChatRoom createRoom(String name) {
+    public ChatRoom createRoomIfNotExist(String location) {
+        Optional<ChatRoomEntity> chatOptional=chatRoomRepository.findByLocation(location);
+        if (chatOptional.isPresent()) {
+            return chatRooms.get(location);
+        } else {
+            ChatRoom createdChatRoom = createRoom(location);
+            return createdChatRoom;
+        }
+    }
+
+    public ChatRoom createRoom(String location) {
         String randomId = UUID.randomUUID().toString();
         ChatRoom chatRoom = ChatRoom.builder()
                 .roomId(randomId)
-                .name(name)
+                .location(location)
                 .build();
         chatRooms.put(randomId, chatRoom);
         return chatRoom;
+    }
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try {
+            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
+        } catch (IOException e) {
+//            log.error(e.getMessage(), e);
+        }
     }
 }
